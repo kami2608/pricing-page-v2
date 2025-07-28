0d5a77f9ca1f9eaae5e76cf7b4971f30a61e100c
dang2608 <gianggiangg2608@gmail.com>
2025-07-28 09:51:38 +0700

    fix: fix camelCase, check valid with regex

--- handle-form.js
+++ handle-form.js
@@ -5,30 +5,36 @@ const repasswordInput = document.getElementById("repassword");
 
 var validate = 0;
 
-const check_special_chars = (str) => {
-  const special_char = "!@#$%^&*()_-+=,.?";
-  return str.split("").some((char) => special_char.includes(char));
+const checkSpecialChars = (str) => {
+  // const special_char = "!@#$%^&*()_-+=,.?";
+  // return str.split("").some((char) => special_char.includes(char));
+  const specialChar = /[!@#$%^&*()_\-+=,.?]/;
+  return specialChar.test(str);
+};
+
+const checkChars = (regexChars, str) => {
+  return regexChars.test(str);
 };
 
 usernameInput.addEventListener("input", () => {
   const username = usernameInput.value.trim();
-  const error_msg = document.getElementById("username-error-msg");
-  error_msg.textContent = check_special_chars(username)
+  const errorMsg = document.getElementById("username-error-msg");
+  errorMsg.textContent = checkSpecialChars(username)
     ? "Username must not contain special characters"
     : "";
-  validate = error_msg.textContent == "" ? 1 : 0;
+  validate = errorMsg.textContent == "" ? 1 : 0;
 });
 
 emailInput.addEventListener("input", () => {
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const email = emailInput.value.trim();
-  const error_msg = document.getElementById("email-error-msg");
-  error_msg.textContent = emailRegex.test(email) ? "" : "Invalid email format";
-  validate = error_msg.textContent == "" ? 1 : 0;
+  const errorMsg = document.getElementById("email-error-msg");
+  errorMsg.textContent = emailRegex.test(email) ? "" : "Invalid email format";
+  validate = errorMsg.textContent == "" ? 1 : 0;
 });
 
 passwordInput.addEventListener("input", () => {
-  const error_msg = document.getElementById("password-error-msg");
+  const errorMsg = document.getElementById("password-error-msg");
   const password = passwordInput.value.trim();
   const errors = [];
   if (password.length < 8)
@@ -38,21 +44,21 @@ passwordInput.addEventListener("input", () => {
   if (!/[a-z]/.test(password))
     errors.push("Password must contain one lowercase letter");
   if (!/[0-9]/.test(password)) errors.push("Password must contain one digit");
-  if (!check_special_chars(password))
+  if (!checkSpecialChars(password))
     errors.push("Password must contain one special characters");
   if (/\s/.test(password)) errors.push("Password must not contain spaces");
-  error_msg.innerHTML = errors.length > 0 ? errors.join("<br>") : "";
-  validate = error_msg.textContent == "" ? 1 : 0;
+  errorMsg.innerHTML = errors.length > 0 ? errors.join("<br>") : "";
+  validate = errorMsg.textContent == "" ? 1 : 0;
 });
 
 repasswordInput.addEventListener("input", () => {
-  const error_msg = document.getElementById("repassword-error-msg");
-  error_msg.textContent =
+  const errorMsg = document.getElementById("repassword-error-msg");
+  errorMsg.textContent =
     repasswordInput.value == passwordInput.value ? "" : "Password not match";
-  validate = error_msg.textContent == "" ? 1 : 0;
+  validate = errorMsg.textContent == "" ? 1 : 0;
 });
 
-document.getElementById("submit-btn").addEventListener("click", () => {
+document.getElementById("register-form").addEventListener("submit", () => {
   if (validate == 1) {
     const userData = {
       username: usernameInput.value,
