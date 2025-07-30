4bfe51757866f6ed5408a60a3d1b3aab9257bdb5
dang2608 <gianggiangg2608@gmail.com>
2025-07-30 09:21:17 +0700

    feat: add task to to-do list

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
+var _a;
+// variables, enums, interfaces
 var Status;
 (function (Status) {
     Status["TODO"] = "TODO";
     Status["PROGRESS"] = "PROGRESS";
     Status["DONE"] = "DONE";
 })(Status || (Status = {}));
-var task1 = {
-    id: 1,
-    title: "ABC",
-    description: "xyz",
-    status: Status.TODO,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-};
-var task2 = {
-    id: 2,
-    title: "XYZ",
-    description: "xyz",
-    status: Status.TODO,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-};
-var todoData = [];
-todoData.push(task1);
-todoData.push(task2);
-localStorage.setItem("todos", JSON.stringify(todoData));
-var localData = JSON.parse(localStorage.getItem("todos") || "");
-var todoTable = document.getElementById("todolist");
-if (todoTable && Array.isArray(localData)) {
-    var rows_1 = "";
-    localData.forEach(function (task) {
-        rows_1 += "\n      <tr>\n        <td>".concat(task.id, "</td>\n        <td>").concat(task.title, "</td>\n        <td>").concat(task.description, "</td>\n        <td>").concat(task.status, "</td>\n        <td>").concat(task.createdAt.toLocaleString(), "</td>\n        <td>").concat(task.updatedAt.toLocaleString(), "</td>\n      </tr>\n    ");
-    });
-    todoTable.innerHTML = rows_1;
+var tasks = [];
+//functions
+function displayTasks() {
+    tasks = JSON.parse(localStorage.getItem("todos") || "[]");
+    var todoTable = document.getElementById("todolist");
+    if (todoTable && Array.isArray(tasks)) {
+        var rows_1 = "";
+        tasks.forEach(function (task) {
+            rows_1 += "\n      <tr>\n        <td>".concat(task.id, "</td>\n        <td>").concat(task.title, "</td>\n        <td>").concat(task.description, "</td>\n        <td>").concat(task.status, "</td>\n        <td>").concat(task.createdAt, "</td>\n        <td>").concat(task.updatedAt, "</td>\n      </tr>\n    ");
+        });
+        todoTable.innerHTML = rows_1;
+    }
 }
+function nextId(todolist) {
+    if (todolist.length > 0)
+        return todolist.length + 1;
+    else
+        return 1;
+}
+function getInput(field) {
+    var inputElement = document.getElementById(field);
+    if (inputElement) {
+        return inputElement.value;
+    }
+    else
+        return "";
+}
+// main
+displayTasks();
+(_a = document.getElementById("add-form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function () {
+    var _a;
+    var title = getInput("title");
+    var description = getInput("description");
+    if (title && description) {
+        var task = {
+            id: nextId(tasks),
+            title: title,
+            description: description,
+            status: Status.TODO,
+            createdAt: new Date().toLocaleString("vi-VN"),
+            updatedAt: new Date().toLocaleString("vi-VN"),
+        };
+        tasks.push(task);
+        localStorage.setItem("todos", JSON.stringify(tasks));
+        alert("Added task!");
+        (_a = document.getElementById("add-form")) === null || _a === void 0 ? void 0 : _a.reset();
+        displayTasks();
+    }
+    else {
+        alert("Please fill in the title and description");
+    }
+});
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// variables, enums, interfaces
 enum Status {
   TODO = "TODO",
   PROGRESS = "PROGRESS",
@@ -9,53 +10,69 @@ interface Task {
   title: string;
   description: string;
   status: Status;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt: string;
+  updatedAt: string;
 }
 
-const task1: Task = {
-  id: 1,
-  title: "ABC",
-  description: "xyz",
-  status: Status.TODO,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
+var tasks: Task[] = [];
 
-const task2: Task = {
-  id: 2,
-  title: "XYZ",
-  description: "xyz",
-  status: Status.TODO,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
+//functions
+function displayTasks() {
+  tasks = JSON.parse(localStorage.getItem("todos") || "[]");
 
-const todoData: Task[] = [];
+  const todoTable = document.getElementById("todolist");
 
-todoData.push(task1);
-todoData.push(task2);
-
-localStorage.setItem("todos", JSON.stringify(todoData));
-
-const localData: Task[] = JSON.parse(localStorage.getItem("todos") || "");
-
-const todoTable = document.getElementById("todolist");
-
-if (todoTable && Array.isArray(localData)) {
-  let rows = "";
-  localData.forEach((task: Task) => {
-    rows += `
+  if (todoTable && Array.isArray(tasks)) {
+    let rows = "";
+    tasks.forEach((task) => {
+      rows += `
       <tr>
         <td>${task.id}</td>
         <td>${task.title}</td>
         <td>${task.description}</td>
         <td>${task.status}</td>
-        <td>${task.createdAt.toLocaleString()}</td>
-        <td>${task.updatedAt.toLocaleString()}</td>
+        <td>${task.createdAt}</td>
+        <td>${task.updatedAt}</td>
       </tr>
     `;
-  });
-  todoTable.innerHTML = rows;
+    });
+    todoTable.innerHTML = rows;
+  }
 }
 
+function nextId(todolist: Task[]): number {
+  if (todolist.length > 0) return todolist.length + 1;
+  else return 1;
+}
+
+function getInput(field: string): string {
+  const inputElement = document.getElementById(field) as HTMLInputElement;
+  if (inputElement) {
+    return inputElement.value;
+  } else return "";
+}
+
+// main
+displayTasks();
+document.getElementById("add-form")?.addEventListener("submit", () => {
+  const title = getInput("title");
+  const description = getInput("description");
+  if (title && description) {
+    const task: Task = {
+      id: nextId(tasks),
+      title: title,
+      description: description,
+      status: Status.TODO,
+      createdAt: new Date().toLocaleString("vi-VN"),
+      updatedAt: new Date().toLocaleString("vi-VN"),
+    };
+    
+    tasks.push(task);
+    localStorage.setItem("todos", JSON.stringify(tasks));
+    alert("Added task!");
+    (document.getElementById("add-form") as HTMLFormElement)?.reset();
+    displayTasks();
+  } else {
+    alert("Please fill in the title and description");
+  }
+});
