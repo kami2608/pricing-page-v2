1e27c3c2710043b58de39c352f746a3357df2416
dang2608 <gianggiangg2608@gmail.com>
2025-07-31 13:59:04 +0700

    add

--- main.js
+++ main.js
@@ -1,153 +1,195 @@
-var _a, _b, _c;
-// variables, enums, interfaces
-var Status;
-(function (Status) {
-    Status["TODO"] = "TODO";
-    Status["PROGRESS"] = "PROGRESS";
-    Status["DONE"] = "DONE";
-})(Status || (Status = {}));
-var tasks = JSON.parse(localStorage.getItem("todos") || "[]");
-var editElement = document.getElementById("edit-task");
-var statusElement = document.getElementById("status-filter");
-var editStatusElm = document.getElementById("edit-status");
-var editTitleElm = document.getElementById("edit-title");
-var editDescElm = document.getElementById("edit-description");
-var cancelBtn = document.getElementById("cancel-button");
-var editForm = document.getElementById("edit-form");
-//functions
-function displayTasks(tasks) {
-    var todoTable = document.getElementById("todolist");
-    if (todoTable && Array.isArray(tasks)) {
-        var rows_1 = "";
-        tasks.forEach(function (task) {
-            rows_1 += "\n      <tr>\n        <td>".concat(task.id, "</td>\n        <td>").concat(task.title, "</td>\n        <td>").concat(task.description, "</td>\n        <td>").concat(task.status, "</td>\n        <td>").concat(task.createdAt, "</td>\n        <td>").concat(task.updatedAt, "</td>\n        <td><button onclick=\"editTask(").concat(task.id, ")\">Edit</button></td>\n        <td><button onclick=\"deleteTask(").concat(task.id, ")\">Delete</button></td>\n      </tr>\n    ");
-        });
-        todoTable.innerHTML = rows_1;
-    }
-}
-function renderStatus(elm) {
-    if (elm) {
-        var selects = "";
-        for (var status_1 in Status) {
-            selects += "<option value=\"".concat(status_1, "\">").concat(status_1, "</option>");
-        }
-        elm.innerHTML = selects;
-    }
-}
-function deleteTask(id) {
-    tasks = tasks.filter(function (task) { return task.id !== id; });
-    displayTasks(tasks);
-    localStorage.setItem("todos", JSON.stringify(tasks));
-}
-function setValue(text, elm) {
-    if (elm) {
-        elm.value = text;
-    }
-}
-function handleEditForm(task) {
-    editForm.addEventListener("submit", function () {
-        var editedTask = {
-            id: task.id,
-            title: editTitleElm.value,
-            description: editDescElm.value,
-            status: editStatusElm.value,
-            createdAt: task.createdAt,
-            updatedAt: new Date().toLocaleString("vi-VN")
-        };
-        deleteTask(task.id);
-        tasks.push(editedTask);
-        displayTasks(tasks);
-        localStorage.setItem("todos", JSON.stringify(tasks));
-        editElement.style.display = "none";
-    });
-    cancelBtn.addEventListener("click", function () {
-        editElement.style.display = "none";
-    });
-}
-function editTask(id) {
-    if (editElement) {
-        renderStatus(editStatusElm);
-        editElement.style.display = "block";
-        var task = undefined;
-        for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
-            var tmp = tasks_1[_i];
-            if (tmp.id === id) {
-                task = tmp;
-                break;
-            }
-        }
-        if (task) {
-            setValue(task.title, editTitleElm);
-            setValue(task.description, editDescElm);
-            editStatusElm.value = task.status;
-            handleEditForm(task);
-        }
-    }
-}
-function getInput(field) {
-    var inputElement = document.getElementById(field);
-    if (inputElement) {
-        return inputElement.value;
-    }
-    else
-        return "";
-}
-function checkIncludes(str, searchStr) {
-    var tmpStr = str.toLowerCase();
-    var tmpSearchStr = searchStr.toLowerCase();
-    var len = str.length;
-    if (searchStr.length === 0)
-        return true;
-    if (searchStr.length > len)
-        return false;
-    for (var i = 0; i <= len - searchStr.length; i++) {
-        if (tmpStr.substring(i, i + searchStr.length) === tmpSearchStr) {
-            return true;
-        }
-    }
-    return false;
-}
-// main
-if (editElement) {
-    editElement.style.display = "none";
-}
-// render status filter
-renderStatus(statusElement);
-// render tasks list
-displayTasks(tasks);
-// create task
-(_a = document.getElementById("add-form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function () {
-    var _a;
-    var title = getInput("title");
-    var description = getInput("description");
-    if (title && description) {
-        var task = {
-            id: new Date().getTime(),
-            title: title,
-            description: description,
-            status: Status.TODO,
-            createdAt: new Date().toLocaleString("vi-VN"),
-            updatedAt: new Date().toLocaleString("vi-VN"),
-        };
-        tasks.push(task);
-        localStorage.setItem("todos", JSON.stringify(tasks));
-        alert("Added task!");
-        (_a = document.getElementById("add-form")) === null || _a === void 0 ? void 0 : _a.reset();
-        displayTasks(tasks);
-    }
-    else {
-        alert("Please fill in the title and description");
-    }
-});
-(_b = document.getElementById("title-filter")) === null || _b === void 0 ? void 0 : _b.addEventListener("input", function () {
-    var title = getInput("title-filter");
-    var status = getInput("status-filter");
-    var filteredTasks = tasks.filter(function (task) { return checkIncludes(task.title, title) && task.status === status; });
-    displayTasks(filteredTasks);
-});
-(_c = document.getElementById("status-filter")) === null || _c === void 0 ? void 0 : _c.addEventListener("input", function () {
-    var title = getInput("title-filter");
-    var status = getInput("status-filter");
-    var filteredTasks = tasks.filter(function (task) { return checkIncludes(task.title, title) && task.status === status; });
-    displayTasks(filteredTasks);
-});
+// // variables, enums, interfaces
+// enum Status {
+//   TODO = "TODO",
+//   PROGRESS = "PROGRESS",
+//   DONE = "DONE",
+// }
+// interface Task {
+//   id: number;
+//   title: string;
+//   description: string;
+//   status: Status;
+//   createdAt: string;
+//   updatedAt: string;
+// }
+// const tasks: Task[] = JSON.parse(localStorage.getItem("todos") || "[]");
+// console.log(localStorage.getItem("todos"));
+// console.log(typeof(localStorage.getItem("todos")));
+// const editElement = document.getElementById("edit-task") as HTMLElement;
+// const statusElement = document.getElementById("status-filter") as HTMLElement;
+// const editStatusElm = document.getElementById(
+//   "edit-status"
+// ) as HTMLSelectElement;
+// const editTitleElm = document.getElementById("edit-title") as HTMLInputElement;
+// const editDescElm = document.getElementById(
+//   "edit-description"
+// ) as HTMLInputElement;
+// const cancelBtn = document.getElementById("cancel-button") as HTMLButtonElement;
+// const editForm = document.getElementById("edit-form") as HTMLFormElement;
+// //functions
+// const debounce = <T extends unknown[]>(
+//   callback: (...args: T) => void,
+//   delay: number
+// ) => {
+//   let timeoutTimer: ReturnType<typeof setTimeout>;
+//   return (...args: T) => {
+//     clearTimeout(timeoutTimer);
+//     timeoutTimer = setTimeout(() => {
+//       callback(...args);
+//     }, delay);
+//   };
+// };
+// function displayTasks(tasks: Task[]) {
+//   const todoTable = document.getElementById("todolist");
+//   if (todoTable && Array.isArray(tasks)) {
+//     let rows = "";
+//     tasks.forEach((task) => {
+//       rows += `
+//       <tr>
+//         <td>${task.id}</td>
+//         <td>${task.title}</td>
+//         <td>${task.description}</td>
+//         <td>${task.status}</td>
+//         <td>${task.createdAt}</td>
+//         <td>${task.updatedAt}</td>
+//         <td><button onclick="editTask(${task.id})">Edit</button></td>
+//         <td><button onclick="deleteTask(${task.id})">Delete</button></td>
+//       </tr>
+//     `;
+//     });
+//     todoTable.innerHTML = rows;
+//   }
+// }
+// function renderStatus(elm: HTMLElement) {
+//   if (elm) {
+//     let selects = `<option value="">Choose status</option>`;
+//     for (const status in Status) {
+//       selects += `<option value="${status}">${status}</option>`;
+//     }
+//     elm.innerHTML = selects;
+//   }
+// }
+// function deleteTask(id: number) {
+//   const index = tasks.findIndex((task) => task.id === id);
+//   if (index !== -1) {
+//     tasks.splice(index, 1);
+//   }
+//   displayTasks(tasks);
+//   localStorage.setItem("todos", JSON.stringify(tasks));
+// }
+// function setValue(text: string, elm: HTMLInputElement) {
+//   if (elm) {
+//     elm.value = text;
+//   }
+// }
+// function handleEditForm(task: Task) {
+//   editForm.addEventListener("submit", () => {
+//     const editedTask: Task = {
+//       id: task.id,
+//       title: editTitleElm.value,
+//       description: editDescElm.value,
+//       status: editStatusElm.value as Status,
+//       createdAt: task.createdAt,
+//       updatedAt: new Date().toLocaleString("vi-VN"),
+//     };
+//     deleteTask(task.id);
+//     tasks.unshift(editedTask);
+//     displayTasks(tasks);
+//     localStorage.setItem("todos", JSON.stringify(tasks));
+//     editElement.style.display = "none";
+//   });
+//   cancelBtn.addEventListener("click", () => {
+//     editElement.style.display = "none";
+//   });
+// }
+// function editTask(id: number) {
+//   if (editElement) {
+//     renderStatus(editStatusElm);
+//     editElement.style.display = "block";
+//     let task = undefined;
+//     for (const tmp of tasks) {
+//       if (tmp.id === id) {
+//         task = tmp;
+//         break;
+//       }
+//     }
+//     if (task) {
+//       setValue(task.title, editTitleElm);
+//       setValue(task.description, editDescElm);
+//       editStatusElm.value = task.status;
+//       handleEditForm(task);
+//     }
+//   }
+// }
+// function getInput(field: string): string {
+//   const inputElement = document.getElementById(field) as HTMLInputElement;
+//   if (inputElement) {
+//     return inputElement.value;
+//   } else return "";
+// }
+// function checkIncludes(str: string, searchStr: string): boolean {
+//   const tmpStr = str.toLowerCase();
+//   const tmpSearchStr = searchStr.toLowerCase();
+//   const len = str.length;
+//   if (searchStr.length === 0) return true;
+//   if (searchStr.length > len) return false;
+//   for (let i = 0; i <= len - searchStr.length; i++) {
+//     if (tmpStr.substring(i, i + searchStr.length) === tmpSearchStr) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// // main
+// if (editElement) {
+//   editElement.style.display = "none";
+// }
+// // render status filter
+// renderStatus(statusElement);
+// // render tasks list
+// displayTasks(tasks);
+// // create task
+// document.getElementById("add-form")?.addEventListener("submit", () => {
+//   const title = getInput("title");
+//   const description = getInput("description");
+//   if (title && description) {
+//     const task: Task = {
+//       id: new Date().getTime(),
+//       title: title,
+//       description: description,
+//       status: Status.TODO,
+//       createdAt: new Date().toLocaleString("vi-VN"),
+//       updatedAt: new Date().toLocaleString("vi-VN"),
+//     };
+//     tasks.unshift(task);
+//     localStorage.setItem("todos", JSON.stringify(tasks));
+//     alert("Added task!");
+//     (document.getElementById("add-form") as HTMLFormElement)?.reset();
+//     displayTasks(tasks);
+//   } else {
+//     alert("Please fill in the title and description");
+//   }
+// });
+// document.getElementById("title-filter")?.addEventListener(
+//   "input",
+//   debounce(() => {
+//     const title = getInput("title-filter");
+//     const status = getInput("status-filter");
+//     const filteredTasks = tasks.filter(
+//       (task) =>
+//         checkIncludes(task.title, title) &&
+//         (status !== "" ? task.status === status : true)
+//     );
+//     displayTasks(filteredTasks);
+//   }, 1000)
+// );
+// document.getElementById("status-filter")?.addEventListener("input", () => {
+//   const title = getInput("title-filter");
+//   const status = getInput("status-filter");
+//   const filteredTasks = tasks.filter(
+//     (task) =>
+//       checkIncludes(task.title, title) &&
+//       (status !== "" ? task.status === status : true)
+//   );
+//   displayTasks(filteredTasks);
+// });
--- main.ts
+++ main.ts
@@ -14,20 +14,41 @@ interface Task {
   updatedAt: string;
 }
 
-var tasks: Task[] = JSON.parse(localStorage.getItem("todos") || "[]");
+const tasks: Task[] = Array.isArray(JSON.parse(localStorage.getItem("todos") || "[]")) ? JSON.parse(localStorage.getItem("todos") || "[]") : [];
+
+console.log(localStorage.getItem("todos"));
+console.log(typeof(localStorage.getItem("todos")));
 
 const editElement = document.getElementById("edit-task") as HTMLElement;
 const statusElement = document.getElementById("status-filter") as HTMLElement;
-const editStatusElm = document.getElementById("edit-status") as HTMLSelectElement;
+const editStatusElm = document.getElementById(
+  "edit-status"
+) as HTMLSelectElement;
 const editTitleElm = document.getElementById("edit-title") as HTMLInputElement;
-const editDescElm = document.getElementById("edit-description") as HTMLInputElement;
+const editDescElm = document.getElementById(
+  "edit-description"
+) as HTMLInputElement;
 const cancelBtn = document.getElementById("cancel-button") as HTMLButtonElement;
 const editForm = document.getElementById("edit-form") as HTMLFormElement;
 
 //functions
+const debounce = <T extends unknown[]>(
+  callback: (...args: T) => void,
+  delay: number
+) => {
+  let timeoutTimer: ReturnType<typeof setTimeout>;
+
+  return (...args: T) => {
+    clearTimeout(timeoutTimer);
+
+    timeoutTimer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
+
 function displayTasks(tasks: Task[]) {
   const todoTable = document.getElementById("todolist");
-
   if (todoTable && Array.isArray(tasks)) {
     let rows = "";
     tasks.forEach((task) => {
@@ -50,7 +71,7 @@ function displayTasks(tasks: Task[]) {
 
 function renderStatus(elm: HTMLElement) {
   if (elm) {
-    let selects = "";
+    let selects = `<option value="">Choose status</option>`;
     for (const status in Status) {
       selects += `<option value="${status}">${status}</option>`;
     }
@@ -59,7 +80,10 @@ function renderStatus(elm: HTMLElement) {
 }
 
 function deleteTask(id: number) {
-  tasks = tasks.filter((task) => task.id !== id);
+  const index = tasks.findIndex((task) => task.id === id);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+  }
   displayTasks(tasks);
   localStorage.setItem("todos", JSON.stringify(tasks));
 }
@@ -70,7 +94,6 @@ function setValue(text: string, elm: HTMLInputElement) {
   }
 }
 
-
 function handleEditForm(task: Task) {
   editForm.addEventListener("submit", () => {
     const editedTask: Task = {
@@ -79,10 +102,10 @@ function handleEditForm(task: Task) {
       description: editDescElm.value,
       status: editStatusElm.value as Status,
       createdAt: task.createdAt,
-      updatedAt: new Date().toLocaleString("vi-VN")
-    }
+      updatedAt: new Date().toLocaleString("vi-VN"),
+    };
     deleteTask(task.id);
-    tasks.push(editedTask);
+    tasks.unshift(editedTask);
     displayTasks(tasks);
     localStorage.setItem("todos", JSON.stringify(tasks));
     editElement.style.display = "none";
@@ -90,7 +113,7 @@ function handleEditForm(task: Task) {
 
   cancelBtn.addEventListener("click", () => {
     editElement.style.display = "none";
-  })
+  });
 }
 
 function editTask(id: number) {
@@ -98,8 +121,8 @@ function editTask(id: number) {
     renderStatus(editStatusElm);
     editElement.style.display = "block";
     let task = undefined;
-    for(const tmp of tasks) {
-      if(tmp.id === id) {
+    for (const tmp of tasks) {
+      if (tmp.id === id) {
         task = tmp;
         break;
       }
@@ -163,7 +186,7 @@ document.getElementById("add-form")?.addEventListener("submit", () => {
       updatedAt: new Date().toLocaleString("vi-VN"),
     };
 
-    tasks.push(task);
+    tasks.unshift(task);
     localStorage.setItem("todos", JSON.stringify(tasks));
     alert("Added task!");
     (document.getElementById("add-form") as HTMLFormElement)?.reset();
@@ -172,21 +195,27 @@ document.getElementById("add-form")?.addEventListener("submit", () => {
     alert("Please fill in the title and description");
   }
 });
-
-document.getElementById("title-filter")?.addEventListener("input", () => {
-  const title = getInput("title-filter");
-  const status = getInput("status-filter");
-  const filteredTasks = tasks.filter(
-    (task) => checkIncludes(task.title, title) && task.status === status
-  );
-  displayTasks(filteredTasks);
-});
+document.getElementById("title-filter")?.addEventListener(
+  "input",
+  debounce(() => {
+    const title = getInput("title-filter");
+    const status = getInput("status-filter");
+    const filteredTasks = tasks.filter(
+      (task) =>
+        checkIncludes(task.title, title) &&
+        (status !== "" ? task.status === status : true)
+    );
+    displayTasks(filteredTasks);
+  }, 1000)
+);
 
 document.getElementById("status-filter")?.addEventListener("input", () => {
   const title = getInput("title-filter");
   const status = getInput("status-filter");
   const filteredTasks = tasks.filter(
-    (task) => checkIncludes(task.title, title) && task.status === status
+    (task) =>
+      checkIncludes(task.title, title) &&
+      (status !== "" ? task.status === status : true)
   );
   displayTasks(filteredTasks);
 });
